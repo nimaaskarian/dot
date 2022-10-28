144a914e4f1ce6b63507c83711ab271fe1b8e78f
nimaaskarian <nimaas1400@gmail.com>
2022-10-28 16:50:10 +0330

    Added xborders run to wal.sh

--- wal.sh
+++ wal.sh
@@ -4,3 +4,5 @@ wpg -s "$1"
 alpha=bf
 . "$HOME/.cache/wal/colors.sh"
 sed -i -e "s/background = #.*/background = #$alpha$(echo $background | tr -d "#")/g" "$HOME/.config/polybar/config.ini"
+killall xborders
+nohup xborders -c "$HOME/.cache/wal/colors-xborders.json" >/dev/null 2>&1 &
